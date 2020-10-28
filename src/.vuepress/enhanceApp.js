e103eb51dc51c88a95a44829a9558f833737fe2d
clampr <christian.lamprecht@aol.de>
2020-10-28 10:48:47 +0100

    Remove clean URLs

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -3,3 +3,16 @@
  *
  * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
  */
+
+export default ({ router }) => {
+  router.addRoutes([
+    {
+      path: '/api/*',
+      redirect: '/docs/api/*'
+    },
+    {
+      path: '/bulk',
+      redirect: '/docs/bulk'
+    }
+  ])
+}
