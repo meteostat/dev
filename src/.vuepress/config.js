65f6592daf5d91d56ed706f6302a6f7a23496f10
clampr <christian.lamprecht@aol.de>
2020-10-28 10:40:54 +0100

    Clean URLs & Redirects

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -287,6 +287,14 @@ module.exports = {
    */
   plugins: [
     '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom'
+    '@vuepress/plugin-medium-zoom',
+    [
+      'vuepress-plugin-clean-urls',
+      {
+        normalSuffix: '',
+        indexSuffix: '',
+        notFoundPath: '/404.html',
+      },
+    ]
   ]
 }
