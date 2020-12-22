3f714ca0cd911fab1f4d521413568b46b95d9199
clampr <christian.lamprecht@aol.de>
2020-12-22 13:39:43 +0100

    Router Tracking

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -4,11 +4,18 @@
  * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
  */
 
- export default ({
+export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
   siteData // site metadata
 }) => {
-  // ...apply enhancements for the site.
+  Vue.mixin({
+    beforeRouteLeave (to, from, next) {
+      _paq.push(['setCustomUrl', to.fullPath]);
+      _paq.push(['setDocumentTitle', 'Meteostat Developers']);
+      _paq.push(['trackPageView']);
+      next()
+    }
+  })
 }
